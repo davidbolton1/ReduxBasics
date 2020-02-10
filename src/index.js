1ad94a202afcf9779bc5efce35c5143b6ae716f9
David Bolton <smdeox@live.com>
2020-02-10 13:55:58 -0500

    Working version

--- src/index.js
+++ src/index.js
@@ -1,100 +1,116 @@
-import {
-    createStore
-} from 'redux';
+// console.log('it is working. i am a robot')
+
 // "The bank" - state
 // Describe the ideal version of state
-/*
-{
-    amount: 100
-}
+// {
+//     amount: 100
+// }
 
-// If we add 1 to the amount, what would state be?
-{
-    amount: 101
-}
+// // if we added 1 to the amount, what would state look like?
+// {
+//     amount: 101
+// }
 
 
-// "A transaction slip" - action
-{
-    type: 'INCRIMENT'
-}
+// // "A transaction slip" - action
+// {
+//     type: 'INCREMENT'    
+// }
 
-{
-    type: 'DECREMENT'
-}
+// {
+//     type: 'DECREMENT'
+// }
 
-{
-    type: 'Emoji'
-}
-*/
-// "the teller" - reducer function
-// Reducers are always named for the state they manage
-// They always receive the current state and the aciton they're processing
-const INCREMENT = 'INCREMENT' //You can define it here to get rid of typos
-const defaultState = {amount:100};
+// {
+//     type: '🏄'
+// }
 
-// You can write action creator functions to format action objects (to avoid types)
-function actionIncrement(howMuch=1) {
-    return {
-        type: INCREMENT,
-        amount: howMuch
-    }
-}
+// {
+//     amounts: [0]
+// }
+
+// {
+//     amounts: [1]
+// }
+
+// {
+//     type: INCREMENT,
+//     id: 0    
+// }
+
+
+
+import { 
+    createStore
+} from 'redux';
+// "The teller" - reducer function
+// reducers are always named for the state they manage.
+// They always receive the current state and the action
+// they're processing.
+const defaultState = { amounts: [0, 0, 0, 0, 0] };
 
 function counter(state=defaultState, action) {
-    const newState = { ...state};
+    console.log('Somebody called counter()');
+    const newState = { ...state };
 
-    switch(action.type) {
-        case INCREMENT:
-            newState.amount = state.amount + action.amount;
-            break;
-        case 'DECREMENT':
-            newState.amount = state.amount - action.amount;
-            break;
-        default:
-            break;
-    }
-    /*
     if (action.type === 'INCREMENT') {
-    newState.amount = state.amount + action.amount;
+        newState.amounts[action.id] = state.amounts[action.id] + 1;
     } else if (action.type === 'DECREMENT') {
-        newState.amount = state.amount - action.amount;
-    } else { 
-        // no need to do anything
-        // We already made a copy of state to return
+        newState.amounts[action.id] = state.amounts[action.id] - 1;
+    } else {
+      // ... no need to do anything.
+      // we already made a copy of state to return.
     }
-    // Must return the new version of the state
-    */
+    // They *must* return the new version of state.
     return newState;
-    
 }
 
-// You give it a reducter, it gives you a 'store'
-// the store is an object that manages your state using your reducter
-const store = createStore(counter);
-
-// "push notificaitons" - subscribe to changes in the store
+// You give it a reducer, it gives you a "store".
+// The store is an object that manages your state 
+// using your reducer.
+const store = createStore(counter, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+// "Push notifications" - subscribe to changes in the store
 store.subscribe(() => {
-    console.log('The state is now')
-    console.table(store.getState())
+    console.log(`The state is now:`);
+    // console.table();
+    console.table(store.getState());
 });
+
+
+function actionIncrement(id) {
+    return {
+        type: 'INCREMENT',
+        id
+    }
+}
+
+function actionDecrement(id) {
+    return {
+        type: 'DECREMENT',
+        id
+    }
+}
 
 // Let's give the store some actions to process.
-
-store.dispatch(
-    /*
-    {
-    type: INCREMENT,
-    amount: 5
-    }
-    */
-   actionIncrement(10)
-);
+store.dispatch(actionIncrement(0));
+store.dispatch(actionIncrement(0));
+store.dispatch(actionIncrement(0));
+store.dispatch(actionIncrement(2));
+store.dispatch(actionIncrement(2));
+store.dispatch(actionDecrement(0));
+store.dispatch(actionDecrement(1));
+store.dispatch(actionDecrement(4));
 
 
-store.dispatch({
-    type: 'DECREMENT',
-    amount: 2
-});
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
 
+// ReactDOM.render(<App />, document.getElementById('root'));
 
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister(); 
