1b842713091b18d2f8b46183a302c7e3342b8e53
David Bolton <smdeox@live.com>
2020-02-10 10:38:55 -0500

    Added increment and decriment functions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,33 @@
 // "The bank" - state
-// "the teller" - reducer function
+// Describe the ideal version of state
+{
+    amount: 100
+}
+
+// If we add 1 to the amount, what would state be?
+{
+    amount: 101
+}
+
+
 // "A transaction slip" - action
+{
+    type: 'INCRIMENT'
+}
+
+{
+    type: 'DECREMENT'
+}
+
+// "the teller" - reducer function
+// Reducers are always named for the state they manage
+// They always receive the current state and the aciton they're processing
+function counter(state, action) {
+    const newState = { ...state};
+    newState.amount = state.amount + 1;
+    // Must return the new version of the state
+    return newState
+}
+
 // "push notificaitons" - subscribe to changes in the store
+
